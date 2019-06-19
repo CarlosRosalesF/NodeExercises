67fe9b88058f75a2265ac4c917931fb04d1ee37d
Carlos Jose Rosales Figueroa <Carlos.Rosales@OMNICON.CC>
2019-06-18 19:05:00 -0500

    Completed making modular exercise

--- MakeItModular.js
+++ MakeItModular.js
@@ -1,1 +1,14 @@
-let filteredLS = require('./utilities/')
+let filteredLS = require('./lib/FilteredLSModule')
+
+
+filteredLS (process.argv[2],process.argv[3], (err, arr)=>{
+
+    if(err){
+        console.error("There was an error",err)
+        return
+    }
+
+    arr.forEach(file => {
+        console.log(file)
+    });
+})
