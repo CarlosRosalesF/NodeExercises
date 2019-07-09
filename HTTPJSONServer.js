7ce81df785aadba3c7184eb6d91790ef8309eca7
Carlos Rosales <carlos.rosales0615@gmail.com>
2019-07-08 23:20:03 -0500

    Finish last exercise HTTP JSON server

--- HTTPJSONServer.js
+++ HTTPJSONServer.js
@@ -18,16 +18,61 @@ http.createServer(function(req, res) {
             jsonDate.minute = isoDate.getMinutes()
             jsonDate.second = isoDate.getSeconds()
             
-            
+            res.end(JSON.stringify(jsonDate))
         }
 
         if (parsedUrl.pathname == '/api/unixtime') {
 
             let isoDate = Date.parse(parsedUrl.query.iso)
+            let jsonDate = {}
+
+            jsonDate.unixtime = isoDate
             
-            res.end(isoDate)
+            res.end(JSON.stringify(jsonDate))
         }
-        
     }
-  
+
+    return res.end('Send me a GET\n')
 }).listen(process.argv[2])
+
+/*
+ Here's the official solution in case you want to compare notes:
+
+─────────────────────────────────────────────────────────────────────────────
+
+    var http = require('http')
+    var url = require('url')
+
+    function parsetime (time) {
+      return {
+        hour: time.getHours(),
+        minute: time.getMinutes(),
+        second: time.getSeconds()
+      }
+    }
+
+    function unixtime (time) {
+      return { unixtime: time.getTime() }
+    }
+
+    var server = http.createServer(function (req, res) {
+      var parsedUrl = url.parse(req.url, true)
+      var time = new Date(parsedUrl.query.iso)
+      var result
+
+      if (/^\/api\/parsetime/.test(req.url)) {
+        result = parsetime(time)
+      } else if (/^\/api\/unixtime/.test(req.url)) {
+        result = unixtime(time)
+      }
+
+      if (result) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(result))
+      } else {
+        res.writeHead(404)
+        res.end()
+      }
+    })
+    server.listen(Number(process.argv[2]))
+*/
